6a39af9597f1a3c64fe52e7927a0b27db794fb36
Oleksandr Zabo <zoleksandr06@gmail.com>
2026-01-29 21:36:24 +0200

    Add project files.

---
+++ WEB_API/Controllers/UsersController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        private static readonly List<User> Users = new List<User>();
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateUserRequest request) 
+        {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            var id = Guid.NewGuid();
+            var user = new User
+            {
+                Id = id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Age = request.Age
+            };
+
+            Users.Add(user);
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id },
+                user
+                );
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] CreateUserRequest request, Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = Users.Where(x => x.Id == id).First();
+            if (user == null)
+            {
+                return BadRequest(ModelState);
+            }
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Age = request.Age;
+            
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id },
+                user
+                );
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id) 
+        {
+            var user = Users.Where(x => x.Id == id).First();
+            if (user == null) { 
+                return BadRequest(ModelState);
+            }
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Users.ToList());
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var user = Users.Where(x => x.Id == id).First();
+            if (user == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Users.Remove(user);
+            return GetAll();
+        }
+
+    }
+}
---
+++ WEB_API/CreateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WEB_API
+{
+    public class CreateUserRequest
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
